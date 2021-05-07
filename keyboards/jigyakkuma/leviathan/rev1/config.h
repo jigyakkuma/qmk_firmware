18f2d51fa06600778d5e6ea9bed8afd93f827447
jigyakkuma <jigyakkuma@gmail.com>
2021-05-07 09:00:39 +0900

    add tapping toggle

--- keyboards/jigyakkuma/leviathan/rev1/config.h
+++ keyboards/jigyakkuma/leviathan/rev1/config.h
@@ -157,3 +157,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 //#define BOOTMAGIC_LITE_ROW 0
 //#define BOOTMAGIC_LITE_COLUMN 0
+
+/* Hardware option */
+#define TAPPING_TOGGLE 2
--- keyboards/jigyakkuma/leviathan/rev1/keymaps/default/keymap.c
+++ keyboards/jigyakkuma/leviathan/rev1/keymaps/default/keymap.c
@@ -34,24 +34,24 @@ enum encoder_number {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
    [_BASE] = LAYOUT(
-   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_LBRC,
    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
-   KC_LGUI, MO(_FN1),         MO(_FN2),KC_LSFT, KC_SPC,      KC_ENT,  KC_RSFT, MO(_FN1),         MO(_FN2),KC_RGUI,
+   KC_LGUI, TT(_FN1),         TT(_FN2),KC_LSPO, KC_SPC,      KC_ENT,  KC_RSPC, KC_BSPC,          KC_RGUI, TT(_FN1),
                                                 _______
    ),
 
    [_FN1] = LAYOUT(
-   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_ZKHK,     KC_1,    KC_2,    KC_3,    KC_0,    KC_MINS, KC_EQL,
-   KC_F12,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TILD,     KC_4,    KC_5,    KC_6,    _______, KC_BSLS, KC_RBRC,
-   _______, KC_F9,   KC_F10,  _______, _______, _______,     KC_7,    KC_8,    KC_9,    _______, _______, _______,
-   _______, _______,          _______, _______, KC_MUTE,     _______, _______, _______,          _______, _______,
+   _______,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+   _______, _______, _______, _______, _______, KC_ZKHK,     KC_TILD, _______, _______, _______, KC_BSLS, KC_RBRC,
+   _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
+   _______, _______,          _______, _______, _______,     _______, _______, _______,          _______, _______,
                                                 _______
    ),
 
    [_FN2] = LAYOUT(
-   _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
-   _______, _______, _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+   KC_F11 , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+   KC_F12 , _______, _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
    _______, _______, _______, _______, _______, _______,     _______, KC_HOME, KC_END,  _______, _______, _______,
    _______, _______,          _______, _______, _______,     _______, KC_PGDN, KC_PGUP,          _______, _______,
                                                 _______
@@ -63,22 +63,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void encoder_update_user(uint8_t index, bool clockwise) {
-    switch (index) {
-      case _1ST_ENC:
-        if (clockwise) {
-            if(get_mods() & MOD_LSFT) {
-                tap_code(KC_VOLU);
-            } else {
-                tap_code(KC_PPLS);
-            }
-        } else {
-             if(get_mods() & MOD_LSFT) {
-                tap_code(KC_VOLD);
-            } else {
-                tap_code(KC_MINS);
-            }
-        }
-        break;
+    switch(get_highest_layer(layer_state)) {
+        case _BASE:
+            tap_code(KC_MUTE);
+            break;
+        case _FN1:
+            clockwise ? tap_code(KC_PPLS) : tap_code(KC_MINS);
+            break;
+        case _FN2:
+            clockwise ? tap_code(KC__VOLUP) : tap_code(KC__VOLDOWN);
+            break;
     }
 }
 
